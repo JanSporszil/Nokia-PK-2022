97eef05bf2fccd97391956462509cbe0705b09d6
Michal <michalsob3@gmail.com>
2022-04-02 16:44:35 -0700

    Fixed UT

--- UE/Application/States/ConnectedState.cpp
+++ UE/Application/States/ConnectedState.cpp
@@ -6,6 +6,7 @@ namespace ue
 ConnectedState::ConnectedState(Context &context)
     : BaseState(context, "ConnectedState")
 {
+    context.user.showConnected();
 }
 
 void ConnectedState::handleSMSReceive(uint8_t mode, std::string content)
